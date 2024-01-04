3c7b333083edf660ea82fdbfd1919f2b92646c3f
qt37424 <qt37424@gmail.com>
2024-01-04 22:38:14 +0700

    Commit abc

--- webapp/src/app/shared/components/register/register.component.ts
+++ webapp/src/app/shared/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 import { passwordService } from "../../services/password.service";
+import { environment } from 'src/environments/environment';
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -25,7 +26,9 @@ export class RegisterAccountComponent implements OnInit {
     // this.submitted = false;
   }
 
+  /// Try to rewrite here to connect database from Golang -- QuangTT12: TODO
   onSubmit(resetForm: NgForm) {
+    console.log("TRAN TRINH QUANG")
     var serchfind: boolean;
 
     this.email = resetForm.value.recoveremail;
@@ -38,7 +41,7 @@ export class RegisterAccountComponent implements OnInit {
 
     if (serchfind === true) {
       this.isLoading = true;
-      this.recover(this.email);
+      this.sendRequestCreateAccount(this.email);
     } else {
       this.message = "Make sure you are entering correct email address";
       this.submitted = true;
@@ -47,6 +50,32 @@ export class RegisterAccountComponent implements OnInit {
   }
 
   private recover(email: string): void {
+    this.passwordService.sendRecovery(email).subscribe(
+      (response) => {
+        console.log(response);
+        if (response === "Request processed.") {
+          this.message =
+            "Your request has been processed. If the email matches our records, you will receive an email. Please check the Spam folder if the email response was not received within 12 hours.";
+          this.form.reset();
+          this.isLoading = false;
+        } else {
+          this.message =
+            "We could not process your request. Make sure you are entering correct email address";
+          this.isLoading = false;
+        }
+      },
+      (error: any) => {
+        this.message =
+          "Unfortunately, we encountered an error. Please check your internet connection or try again later";
+        this.form.reset();
+        this.isLoading = false;
+      }
+    );
+    this.submitted = true;
+  }
+
+  /// I need to rewrite it to createAccount
+  private sendRequestCreateAccount(email: string): void {
     this.passwordService.sendRecovery(email).subscribe(
       (response) => {
         console.log(response);
